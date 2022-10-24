4e894c959a1c475f62c895e1a12cc12a417bae52
akhilyarlagadda9 <akhilyarlagadda9@gmail.com>
2022-10-24 18:10:11 -0500

    adding items to cart

---
+++ MobileTruckFood/Controller/CartTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  CartTableViewCell.swift
+//  MobileTruckFood
+//
+//  Created by Student on 14/09/2022.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    @IBOutlet var contantView: UIView!
+    @IBOutlet var checkBtn: UIButton!
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBOutlet var nameLBL: UILabel!
+    @IBOutlet var ratingLBL: UILabel!
+    
+    @IBOutlet var minusBtn: UIButton!
+    @IBOutlet var counterLBL: UILabel!
+    @IBOutlet var plusBtn: UIButton!
+    
+    @IBOutlet var priceLBL: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
