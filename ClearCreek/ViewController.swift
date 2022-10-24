45e7eac41511b3acc5b72d40b18cefda31ad4189
akhilyarlagadda9 <akhilyarlagadda9@gmail.com>
2022-10-24 17:54:19 -0500

    View Splash LogIn Controller by Akhil

---
+++ ClearCreek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearCreek
+//
+//  Created by Student on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
